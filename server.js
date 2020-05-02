f7b0b5c84f67c3044c42bc0328e2445e846b6a1f
Inna Widener <iwide2019@gmail.com>
2020-05-01 21:35:32 -0500

    Npm dotenv added. Created heroku database. Deployment.

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const router = require("./routes/html-routes");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const PORT = 3000;
 
 const app = express();
+app.use(compression());
+
+const Transaction = require("./models/transaction.js");
 
 app.use(logger("dev"));
 
@@ -15,13 +21,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI,
+ {
   useNewUrlParser: true,
   useFindAndModify: false
-});
+})
+.then(() => console.log("DB Connected!"));
 
 // routes
 app.use(require("./routes/api.js"));
+router(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
