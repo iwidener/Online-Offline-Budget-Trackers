96da5a01e87bc1ada9d15b03147fdbd066e4728a
Inna Widener <iwide2019@gmail.com>
2020-05-07 22:05:16 -0500

    Changed line 9

--- server.js
+++ server.js
@@ -2,29 +2,28 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-//const router = require("./routes/html-routes");
+// const router = require("./routes/html-routes");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(compression());
 
 const Transaction = require("./models/transaction.js");
 
+// middleware
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI || "mongodb://localhost/budget",
  {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 })
 .then(() => console.log("DB Connected!"));
 
